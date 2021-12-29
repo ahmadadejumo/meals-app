0eec2fb13c3665e17b0180f2544552ab72580973
ahmadadejumo <adejumoahmad@gmail.com>
2021-12-29 12:59:33 +0100

    converted main.dart into a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ import './screens/meal_deatail_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
